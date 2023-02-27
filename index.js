3cbc2db6f6c156c4ec8cbac189027e9bbe9a752e
Tsimafey Kovzhun <kovzhun03@gmail.com>
2023-02-27 03:08:21 +0300

    промежуточный backend auth

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+import { registerValidation } from './validations/auth.js';
+import { validationResult } from 'express-validator';
+import UserModel from './models/User.js';
+
+mongoose.set('strictQuery', false);
+mongoose
+.connect('mongodb+srv://admin:wwwwww@cluster0.eh4mr1g.mongodb.net/?retryWrites=true&w=majority')
+.then(() => console.log('DB ok'))
+.catch((err) => console.log('DB error', err));
+
+const app = express();
+
+app.use(express.json());
+
+
+app.post('/auth/register', registerValidation, async (req, res ) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash: req.body.password,
+    });
+
+    const user = await doc.save();
+
+    res.json({user});
+});
+
+app.listen(4444, (err) => {
+    if(err){
+        return console.log(err);
+    }
+
+    console.log('Serer OK');
+});
