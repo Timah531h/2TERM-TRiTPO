09de3412202d7772aecf9740c2718963fd2ff892
Tsimafey Kovzhun <kovzhun03@gmail.com>
2023-04-05 04:10:05 +0300

    Обновление функционала backend

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { loginValidation, registerValidation } from './validations.js';
+import { loginValidation, registerValidation, postCreateValidation } from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
 
 
 mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.fnp9ooh.mongodb.net/blog?retryWrites=true&w=majority'
@@ -17,6 +18,17 @@ app.post('/auth/login',loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register);
 app.get('/auth/me', checkAuth,UserController.getMe);
 
+
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch(
+    '/posts/:id',
+    checkAuth,
+    postCreateValidation,
+    PostController.update,
+  );
 
 
 app.listen(4444, (err) => {
